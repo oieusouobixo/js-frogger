fdf74b9a373bd7056921c322e42dded058518321
oieusouobixo <guicosta87@yahoo.com.br>
2020-10-02 17:45:46 -0300

    basic files

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const SQUARES = document.querySelectorAll('.grid div')
+  const TIME_LEFT = document.querySelector('#time-left')
+  const RESULT = document.querySelector('#result')
+  const START_BTN = document.querySelector('#button')
+  const WIDTH = 9
+  let currentIndex = 76
+})
